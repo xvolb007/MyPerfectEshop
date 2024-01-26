cdd7078b63cb9447b1fc3db13d51c879d9642f06
xvolb007 <xvolb007@studenti.czu.cz>
2024-01-26 21:39:18 +0200

    Before N-Tier

--- EshopBooks/Controllers/CategoryController.cs
+++ EshopBooks/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using EshopBooks.Data;
 using EshopBooks.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
 
 namespace EshopBooks.Controllers
 {
@@ -31,9 +32,65 @@ namespace EshopBooks.Controllers
             {
 				_db.Categories.Add(obj);
 				_db.SaveChanges();
+				TempData["success"] = "Category created successfully";
 				return RedirectToAction("Index", "Category");
 			}
                 return View();
         }
-    }
+		public IActionResult Edit(int? id)
+		{
+            if (id==null || id ==0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Просто можно находить не только по примарному ключу как у файнда
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.DisplayOrder==id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+			return View(categoryFromDb);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				TempData["success"] = "Category updated successfully";
+				return RedirectToAction("Index", "Category");
+			}
+			return View();
+		}
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.Find(id);
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost, ActionName("Delete")]
+
+		public IActionResult DeletePost(int ? id)
+		{
+			Category ? obj = _db.Categories.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+			TempData["success"] = "Category deleted successfully";
+			return RedirectToAction("Index", "Category");
+		}
+	}
 }
