9ec6b0cf59400736b96dff3218ae8f3758988d23
xvolb007 <unicartoon2002@gmail.com>
2024-02-11 15:35:59 +0100

    Added repository pattern

---
+++ Eshop.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using Eshop.DataAccess.Repository.IRepository;
+using Eshop.Models;
+using EshopBooks.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db):base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _db.Categories.Update(category);
+        }
+    }
+}
--- EshopBooks/Program.cs
+++ EshopBooks/Program.cs
@@ -1,3 +1,5 @@
+using Eshop.DataAccess.Repository;
+using Eshop.DataAccess.Repository.IRepository;
 using EshopBooks.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
